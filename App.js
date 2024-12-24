c94e7dfcbd94ebec688944bc0f883b284d8df687
FATHIMA SAHLA <159443719+FathimaSahlaAS@users.noreply.github.com>
2024-12-24 21:44:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+import 'react-native-get-random-values';
+
+const App = () => <AppNavigator />;
+
+export default App;
